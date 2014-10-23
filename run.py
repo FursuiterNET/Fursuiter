2a787ba4f2b395470de4e60d2d9739d9dd51e302
Zac Sturgeon <jkltechinc@gmail.com>
2014-10-23 01:28:24 -0400

    Change local WSGI server to prefer Werkzeug to have nice things like an inline debugger and an autreloader.  Just append 'debug = true' to your config.ini and install Werkzeug to take advantage.

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from wsgiref.simple_server import make_server, WSGIServer
+#!/usr/bin/env python
+#*-* coding: utf-8 *-*
+# Local server will attempt to run using Werkzeug first if available, or
+# will fail to a generic WSGIServer. Werkzeug offers niceties like a
+# a debugging console and autorealoder.
+
 try:
     from socketserver import ThreadingMixIn
 except ImportError:
@@ -11,12 +16,25 @@ import os
 
 from Fursuiter import main
 
-
-class ThreadedWSGIServer(ThreadingMixIn, WSGIServer):
-    pass
-
+    
 parser = SafeConfigParser({'here': os.path.dirname(os.path.abspath(__file__))})
 parser.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini'))
-application = main(**dict(parser.items('app:main')))
-server = make_server('', 5000, application, server_class=ThreadedWSGIServer)
-server.serve_forever()
+config = dict(parser.items('app:main'))
+
+application = main(**config)
+DEBUG = config.get('debug')
+    
+if __name__ == '__main__':
+    try:
+        from werkzeug.serving import run_simple
+        if DEBUG is not None and DEBUG.lower() == "true":
+            from werkzeug.debug import DebuggedApplication
+            application = DebuggedApplication(application, evalex=True)
+        run_simple('localhost', 5000, application, use_reloader=DEBUG)
+        
+    except ImportError:
+        from wsgiref.simple_server import make_server, WSGIServer
+        class ThreadedWSGIServer(ThreadingMixIn, WSGIServer):
+            pass
+        server = make_server('', 5000, application, server_class=ThreadedWSGIServer)
+        server.serve_forever()
